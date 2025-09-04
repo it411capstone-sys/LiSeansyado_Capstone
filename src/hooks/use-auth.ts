57132117a73433f19cb11df69f30a70a62698f1c
Lea Medrano <it411capstone@gmail.com>
2025-09-04 06:59:07 +0000

    how can we fetch user's first name and last name in user registration?

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,8 +1,6 @@
 
 'use client';
 import { useContext } from 'react';
-// Correct the path based on your file structure
-// Assuming auth-context.tsx is in src/contexts/
 import { AuthContext } from '@/contexts/auth-context';
 
 export const useAuth = () => {
