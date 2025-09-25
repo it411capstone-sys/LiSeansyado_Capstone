708bf7ec89cf9e1820361ce4eea56fbc90f493b8
Lea Medrano <it411capstone@gmail.com>
2025-09-25 07:55:02 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,24 +1,7 @@
 
-'use client';
-
 import { AdminHeader } from "@/components/admin/admin-header";
-import { LanguageToggle } from "@/components/language-toggle";
-import { Suspense } from "react";
-import { UserNav } from "@/components/user-nav";
 import { InspectionsProvider } from "@/contexts/inspections-context";
-
-function AdminLayoutContent({ children }: { children: React.ReactNode }) {
-  return (
-      <div className="flex min-h-screen w-full flex-col">
-        <AdminHeader />
-        <main className="flex flex-1">
-          <div className="flex flex-col flex-1 bg-muted/40">
-            {children}
-          </div>
-        </main>
-      </div>
-  );
-}
+import { Suspense } from "react";
 
 export default function AdminLayout({
   children,
@@ -26,10 +9,17 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <InspectionsProvider>
-          <AdminLayoutContent>{children}</AdminLayoutContent>
-        </InspectionsProvider>
-      </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <InspectionsProvider>
+        <div className="flex min-h-screen w-full flex-col">
+          <AdminHeader />
+          <main className="flex flex-1">
+            <div className="flex flex-col flex-1 bg-muted/40">
+              {children}
+            </div>
+          </main>
+        </div>
+      </InspectionsProvider>
+    </Suspense>
   );
 }
